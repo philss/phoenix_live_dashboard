0f6da1792796664563493f04cee47b5a888def96
Philip Sampaio <philip.sampaio@gmail.com>
2021-09-08 18:36:28 -0300

    Add health controller
    This controller is necessary to check if PLDS is running.

--- plds/lib/plds_web/router.ex
+++ plds/lib/plds_web/router.ex
@@ -5,6 +5,8 @@ defmodule PLDSWeb.Router do
   scope "/" do
     pipe_through [:fetch_session, :protect_from_forgery]
 
+    get "/health", PLDSWeb.HealthController, :index
+
     live_dashboard "/",
       metrics: PLDSWeb.Telemetry,
       additional_pages: [broadway: BroadwayDashboard]
